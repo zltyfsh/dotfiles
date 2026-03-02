41fc890cadb88146e9c580e82184da8973d5ccb6
Per Carlson <per.carlson@vegvesen.no>
2026-03-02 12:03:09 +0100

    Updated ls aliases

--- 00-bash_aliases.sh
+++ 00-bash_aliases.sh
@@ -1,8 +1,5 @@
-# reset any other custom ls alias
-unalias ls
-alias ls='ls --color=auto'
 alias ll='ls -lF'
-alias la='ll -A'
+alias la='la -lAF'
 
 alias tr1='tree -L 1'
 alias tr2='tree -L 2'
@@ -10,8 +7,12 @@ alias tr3='tree -L 3'
 
 # If we have ack-grep, but not ack, alias ack to ack-grep
 if command -v ack-grep >/dev/null && ! command -v ack >/dev/null; then
-  alias ack="$(command -v ack-grep)"
+	alias ack="$(command -v ack-grep)"
 fi
 
-# source local bash files
-[[ -f $HOME/.local/bash_aliases ]] && source $HOME/.local/bash_aliases
+# Install kubectl alias, if we have kubectl...
+if command -v kubectl >/dev/null; then
+	alias k=kubectl
+	alias ks='kubectl get events --sort-by metadata.creationTimestamp'
+
+	# This is strict
